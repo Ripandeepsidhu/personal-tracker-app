6b55eb5233c660e561f64dbc092b4b834788ba44
Ripandeepsidhu <kaursidhuripandeep@gmail.com>
2024-09-13 20:58:32 +0200

    updated todo component

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,16 +1,19 @@
 import React, {useState} from 'react'
 import { TodoForm } from './TodoForm'
-import{v4 as uuid}
+import {v4 as uuid} from 'uuid'
+import { Todo } from './Todo';
+uuidv4();
 
 export const TodoWrapper = () => {
     const [todos, setTodos] = useState([])
 
     const addTodo = todo => {
-        setTodos([...todos, {id:uuidv4(), task:todo, completed:false,isEditing:false}])
+        setTodos([...todos, {id: uuidv4(), task:todo, completed:false,isEditing:false}])
     }
   return (
     <div className='TodoWrapper'>
         <TodoForm addTodo={addTodo}/>
+        <Todo/>
     </div>
   )
 }
